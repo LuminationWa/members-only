082ffe8fcf201348a8053a2b9d1c96905be7acfd
Edel <edelmedina322@gmail.com>
2023-01-23 17:59:40 -0300

    Initial commit

---
+++ models/post.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  date: { type: Date, required: true },
+  text: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+// Virtual for post's URL
+PostSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/posts/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Post", PostSchema);
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  membership: { type: Boolean, required: true },
+});
+
+// Virtual for user's URL
+UserSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/users/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("User", UserSchema);
