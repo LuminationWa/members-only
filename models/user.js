e59571cdcaff4d61ca9d798bd405195248b3efc5
Edel <edelmedina322@gmail.com>
2023-02-01 21:01:56 -0300

    Add validation / hashing / user creation

--- models/user.js
+++ models/user.js
@@ -7,7 +7,9 @@ const UserSchema = new Schema({
   last_name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  membership: { type: Boolean},
+  membership: { type: Boolean },
+}, {
+  collection: "Users"
 });
 
 // Virtual for user's URL
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,5 +11,41 @@ router.get("/", function (req, res, next) {
 router.get("/sign-up", function (req, res, next) {
   res.render("sign-up");
 });
+router.post(
+  "/sign-up",
+  [
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters"),
+    check("confirm-password").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password confirmation does not match password");
+      }
+      return true;
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      }
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+      }).save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/");
+      });
+    });
+  }
+);
 
 module.exports = router;
